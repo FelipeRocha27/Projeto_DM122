bec7faac0e234cad410332df4485f90b405e9cda
unknown <felipe.gustavo@pg.inatel.br>
2022-11-14 22:09:04 -0300

    mudanca no sw.js

--- public/sw.js
+++ public/sw.js
@@ -7,11 +7,12 @@ self.addEventListener("install", function (e) {
       return cache
         .addAll([
           "./",
-          "/icons/list256.png",
-          "/icons/list512.png",
-          "./manifest.json",
-          "./index.js",
-          "./index.html",
+          "../icons/list256.png",
+          "../icons/list512.png",
+          "../icons/sorvete.png",
+          "/manifest.json",
+          "/index.js",
+          "../index.html",
         ])
         .then(function () {
           console.log("cached!");
